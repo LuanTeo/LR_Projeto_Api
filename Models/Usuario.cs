4212cab3197a80c0e3932688df38e190ed26f397
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2024-11-12 20:07:13 -0400

    Fazendo o model do Usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -2,6 +2,13 @@
 {
     public class Usuario
     {
-
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Genero {  get; set; }
+        public string Email { get; set; }
+        public int Cpf { get; set; }
+        public string Telefone {  get; set; }
+        public string Senha { get; set; }
+        public DateTime Data_Nascimento { get; set; }
     }
 }
